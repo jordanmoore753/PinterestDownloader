b3561936cd540ca4a4987014b84fb82e5a68a17d
Jordan Moore <jmoore@within3.com>
2022-08-18 12:33:11 -0400

    reduce steps

--- sample.js
+++ sample.js
@@ -3,26 +3,16 @@
 Here are the steps for getting all pictures from a board.
 
 The images are loaded asynchronously based on where you are on the page. Images that are not in view are not in the DOM,
-so you need to scroll for each section.
+so you need to scroll for each section of images.
 
-Do these steps in the console in your browser ON the page with the board you want to get pictures for:
+Run the first step in the browser console. Do the second step in the pin.rb file.
 
-e = []
+1. copy(Array.from(document.querySelectorAll('img')).map(image => image.attributes.srcset?.textContent).filter(image => image !== undefined).map(url => url.split(', ')[3].split(' ')[0]))
 
-document.querySelectorAll('img').forEach(d => e.push(d.attributes.srcset?.textContent))
+2. Paste the array into the new_pins method in the pin.rb file. Add a , after where you pasted it.
 
-e = e.filter(ff => ff !== undefined)
+Repeat this process until you've reached the bottom of the page.
 
-e = e.map(ff => ff.split(', ')[3].split(' ')[0])
-
-copy(e)
-
-(After this, you can press the up arrow button to navigate through past commands in the console (so you don't need to keep pasting them)).
-
-Then, paste the array into the new_pins method in the pin.rb file. Add a , after where you pasted it.
-
-Keep repeating all of these steps until you reach the bottom of the board.
-
-Run the program from the command line (in this directory) with: ruby pin.rb.
+Finally, run the program from the command line (in this directory) with: ruby pin.rb.
 
 */
